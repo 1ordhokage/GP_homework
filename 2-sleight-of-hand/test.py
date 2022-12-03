174f6a737cd97b109721360e5188e7fa7ce75914
Jurabek Aminov <dzhurabek.aminov@gmail.com>
2022-12-03 17:22:02 +0300

    Added tests

---
+++ 2-sleight-of-hand/test.py
@@ -0,0 +1,27 @@
+import main
+import unittest
+
+
+class SleightOfHandTest(unittest.TestCase):
+    """Simple testcases. You can add your testcases below."""
+    def test1(self):
+        k = 3
+        field = "1231/n2..2/n2..2/n2..2"
+        result = main.sleight_of_hand(k, field)
+        self.assertEqual(result, 2)
+
+    def test2(self):
+        k = 4
+        field = "1111999911119911"
+        result = main.sleight_of_hand(k, field)
+        self.assertEqual(result, 1)
+
+    def test3(self):
+        k = 4
+        field = "1111111111111111"
+        result = main.sleight_of_hand(k, field)
+        self.assertEqual(result, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
